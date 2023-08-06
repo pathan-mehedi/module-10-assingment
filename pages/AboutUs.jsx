7efc9d1bab186e72bc976b0a4cf2f7d35ef2dc24
Mehedi Pathan <sma.mehedi@gmai.com>
2023-08-06 22:11:57 +0600

    Initial commit

---
+++ pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+// pages/AboutUs.js
+import React from 'react';
+
+export default function AboutUs() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our Counter App!</p>
+      <p>We are a team of developers passionate about creating useful and interactive applications.</p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,58 @@
 import { useState } from 'react';
+import Link from 'next/link';
 import styles from '../styles/styles.module.scss';
 
 export default function Home() {
-  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
+  const increment1 = () => {
+    setCount1(count1 + 1);
   };
 
-  const decrement = () => {
-    if (count > 0) {
-      setCount(count - 1);
+  const decrement1 = () => {
+    if (count1 > 0) {
+      setCount1(count1 - 1);
+    }
+  };
+
+  const increment2 = () => {
+    setCount2(count2 + 1);
+  };
+
+  const decrement2 = () => {
+    if (count2 > 0) {
+      setCount2(count2 - 1);
     }
   };
 
   return (
-    <div className={styles.container}>
-      <h1>Counter App</h1>
-      <p className={styles.counter}>Current Count: {count}</p>
-      <button className={styles.button} onClick={increment}>Increment</button>
-      <button className={styles.button} onClick={decrement}>Decrement</button>
+    <>
+    <header className={styles.header}>
+        <div className={styles.logo}>Counter App</div>
+        <nav className={styles.navMenu}>
+          <Link href="/">Home</Link>
+          <Link href="/AboutUs">About Us</Link>
+        </nav>
+      </header>
+    <div className={styles.motherContainer}>
+      <div className={styles.counterContainer}>
+        <h1 className={styles.head_title}>Counter 1</h1>
+        <p className={styles.counter}>Current Count: {count1}</p>
+        <div className={styles.btn_wrapper}>
+        <button className={styles.button} onClick={increment1}>Increment</button>
+        <button className={styles.button} onClick={decrement1}>Decrement</button>
+        </div>
+      </div>
+      <div className={styles.counterContainer}>
+      <h1 className={styles.head_title}>Counter 2</h1>
+        <p className={styles.counter}>Current Count: {count2}</p>
+        <div className={styles.btn_wrapper}>
+        <button className={styles.button} onClick={increment2}>Increment</button>
+        <button className={styles.button} onClick={decrement2}>Decrement</button>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
